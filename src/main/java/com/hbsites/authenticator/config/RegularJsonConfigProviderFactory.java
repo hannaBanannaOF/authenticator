e7e1a31547b157699bb109bf0103cc952c271b46
Ravi Batistel <ravi.olly@outlook.com>
2023-09-13 17:38:22 -0300

    Docker authenticator

--- src/main/java/com/hbsites/authenticator/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,5 +0,0 @@
-package com.hbsites.authenticator.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory { }
